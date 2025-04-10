04c1c7a3c81cf578f946661dc1d5af3941bc1efa
Official-Krish <krishanand0103@gmail.com>
2025-04-10 16:53:25 +0530

    init

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,17 +1,16 @@
-import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction, TransactionInstruction } from "@solana/web3.js";
 import { test, expect } from "bun:test";
 import { COUNTER_SIZE, schema } from "./types";
 import * as borsh from "borsh";
 
 let CounterAccountKeypair: Keypair = Keypair.generate();
 let adminKeypair: Keypair = Keypair.generate();
+const programId = new PublicKey("2PqBAFFXqy7vuJMkZaferCinMj8ed3RATbsMXMzLwEWY");
+const connection = new Connection("http://localhost:8899");
 
 test("account is initialized", async () => {
-    const connection = new Connection("http://localhost:8899");
     const txn = await connection.requestAirdrop(adminKeypair.publicKey, 2 * LAMPORTS_PER_SOL);
     await connection.confirmTransaction(txn);
-
-    const programId = new PublicKey("B5rTDa7r7NoSkWydncMyaW6Ahms6gMQWc1DeXbKvV6H");
 
     const lamports = await connection.getMinimumBalanceForRentExemption(COUNTER_SIZE);
     const createAccount = SystemProgram.createAccount({
@@ -29,3 +28,41 @@ test("account is initialized", async () => {
     const counter = borsh.deserialize(schema, account?.data!);
     expect(counter?.count).toEqual(0);
 })
+
+test("increment", async () => {
+    const txn =  new Transaction();
+    txn.add(new TransactionInstruction({
+        keys: [{
+            pubkey: CounterAccountKeypair.publicKey,
+            isSigner: false,
+            isWritable: true
+        }],
+        programId,
+        data: Buffer.from(new Uint8Array([0, 1, 0, 0, 0]))
+    }));
+    const txHash = await connection.sendTransaction(txn, [adminKeypair]);
+    await connection.confirmTransaction(txHash);
+
+    const account = await connection.getAccountInfo(CounterAccountKeypair.publicKey);
+    const counter = borsh.deserialize(schema, account?.data!);
+    expect(counter?.count).toBe(1);
+});
+
+test("decrement", async () => {
+    const txn =  new Transaction();
+    txn.add(new TransactionInstruction({
+        keys: [{
+            pubkey: CounterAccountKeypair.publicKey,
+            isSigner: false,
+            isWritable: true
+        }],
+        programId,
+        data: Buffer.from(new Uint8Array([1, 1, 0, 0, 0]))
+    }));
+    const txHash = await connection.sendTransaction(txn, [adminKeypair]);
+    await connection.confirmTransaction(txHash);
+
+    const account = await connection.getAccountInfo(CounterAccountKeypair.publicKey);
+    const counter = borsh.deserialize(schema, account?.data!);
+    expect(counter?.count).toBe(0);
+});
